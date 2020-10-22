611054a5f7651aba02832fddef1f3ebb25449302
sjotwani94 <siddharthkj345@gmail.com>
2020-10-22 18:46:22 +0530

    commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'q=^#&*$7&8^d_mh3x-h_$-qe8s1+nzzxg0i+3@j&s)mjb^6o$)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['employeedjangoapp.herokuapp.com']
+ALLOWED_HOSTS = ['employeedjangoapp.herokuapp.com','localhost']
 
 
 # Application definition
